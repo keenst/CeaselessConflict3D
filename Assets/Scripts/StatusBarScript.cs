d5adeffa48261d26a73117738493360792ab254f
keenst <leo.siven@icloud.com>
2023-11-15 18:51:11 +0100

    Created prefabs for status bars

--- Assets/Scripts/StatusBarScript.cs
+++ Assets/Scripts/StatusBarScript.cs
@@ -4,19 +4,28 @@ using UnityEngine.UI;
 public class StatusBarScript : MonoBehaviour
 {
     public Color fillColor;
-    
+
     [Range(0, 1)] 
     public float fillAmount;
 
     public Image full;
     public Image empty;
 
-    public void SetValue(float value, float max)
+    private float _defaultX;
+    
+    public void Start()
     {
-        
+        _defaultX = full.rectTransform.position.x;
     }
-
+    
     public void Update()
+    {
+        RectTransform rect = full.rectTransform;
+        rect.localScale = new Vector3(fillAmount, 1);
+        rect.position = new Vector3(_defaultX + fillAmount * 180 - 180, rect.position.y);
+    }
+    
+    public void SetValue(float value, float max)
     {
         
     }
